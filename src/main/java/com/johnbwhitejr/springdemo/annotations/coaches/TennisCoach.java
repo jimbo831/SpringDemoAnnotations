38ffc61234bd33d1d4fb71bf5b6ac35feecdb253
jimbo831 <jimbo831@gmail.com>
2016-11-16 22:16:38 -0600

    Support for Qualifier annotation

--- src/main/java/com/johnbwhitejr/springdemo/annotations/coaches/TennisCoach.java
+++ src/main/java/com/johnbwhitejr/springdemo/annotations/coaches/TennisCoach.java
@@ -3,6 +3,7 @@ package com.johnbwhitejr.springdemo.annotations.coaches;
 import com.johnbwhitejr.springdemo.annotations.fortunes.FortuneService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach {
 
     @Autowired
+    @Qualifier("happyFortuneService")
     private FortuneService fortuneService;
 
     @Override
---
+++ src/main/java/com/johnbwhitejr/springdemo/annotations/fortunes/DatabaseFortuneService.java
@@ -0,0 +1,16 @@
+package com.johnbwhitejr.springdemo.annotations.fortunes;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by johnwhite on 11/16/16.
+ */
+
+@Component
+public class DatabaseFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
